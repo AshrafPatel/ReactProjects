de90a2df034c8588768c3e6ed1e1649f0158b165
Ashraf Patel <4shr4f@live.co.uk>
2019-09-06 15:51:52 -0400

    Added Color App to react projects

---
+++ random-colors/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ColorApp from "./ColorApp/ColorApp"
+
+function App() {
+  return (
+    <div className="App">
+      <ColorApp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ random-colors/src/ColorApp/ColorApp.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react"
+
+class ColorApp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            colors: []
+        }
+    }
+
+    render() {
+        return (
+            <div>
+            
+            </div>
+        )
+    }
+}
+
+export default ColorApp
